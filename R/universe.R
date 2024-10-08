71a0f50440f5c4ab1cccd4ea1578e719af0fa6ef
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-08 10:44:54 +0200

    more

--- R/universe.R
+++ R/universe.R
@@ -75,3 +75,33 @@ universe_package <- function(universe, package) {
     path = sprintf("packages/%s", package),
   )
 }
+
+#' Search within a single universe
+#'
+#' @param universe Name of the universe (character of length 1)
+#' @param query Query string. See [R-universe docs](https://docs.r-universe.dev/browse/search.html).
+#' @param limit Number of results to return (integer of length 1)
+#'
+#' @return A list with query results. The `total` field indicates the
+#' total number of results and can be used as `limit` value in a second call.
+#' @export
+#'
+#' @examplesIf interactive()
+#' universe_search("ropensci", query = '"weather data"')
+#' universe_search("ropensci", query = 'needs:httr2')
+#' @family universe
+universe_search <- function(universe, query, limit) {
+  if (!is.character(universe) || length(universe) != 1) {
+    cli::cli_abort("{.arg universe} must be a character of length 1.")
+  }
+  if (!is.character(query) || length(query) != 1) {
+    cli::cli_abort("{.arg query} must be a character of length 1.")
+  }
+  # TODO assert that universe is an universe
+
+  universe_query(
+    universe_url = sprintf("https://%s.r-universe.dev", universe),
+    path = "search",
+    query_params = list(q = query, limit = limit)
+  )
+}
--- tests/testthat/test-universe.R
+++ tests/testthat/test-universe.R
@@ -27,3 +27,11 @@ test_that("universe_package() errors well", {
     universe_package("jeroen", package = "shiny")
   })
 })
+
+test_that("universe_search() works", {
+  httptest2::with_mock_dir("univ-search", {
+    packages <- universe_search("ropensci", query = 'needs:gert', limit = 1)
+  })
+  expect_type(packages, "list")
+  expect_gt(length(packages), 0)
+})
